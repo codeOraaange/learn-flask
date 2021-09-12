2e197c8cb34af8219396882bb9a304527b4ed182
codeOraaange <inihana1@gmail.com>
2021-09-12 21:15:26 +0700

    add: materi part 4

---
+++ #4-rest_jwt_auth/app/__init__.py
@@ -0,0 +1,18 @@
+
+from flask import Flask
+from flask_restful import Api
+from app.config import Config
+
+app = Flask(__name__, template_folder='views')
+app.config.from_object(Config)
+
+api = Api(app, prefix='/api')
+web = Api(app)
+
+from app.db_manager import DatabaseManager
+DatabaseManager.open_database()
+
+from flask_jwt_extended import JWTManager
+jwt = JWTManager(app)
+
+from app import routes
